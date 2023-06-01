8840281736871156aab2728445259a37ba2a4811
Hakkar <zugrutus@gmail.com>
2023-06-01 13:58:54 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import 'manrope';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'components/App';
 import { Provider } from 'react-redux';
-import {store, persistor} from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import {store } from 'redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
 
 import shopsReducer from './reducers/shop';
 import cartReducer from './reducers/cart';
 import orderReducer from "./reducers/order";
-import persistReducer from "redux-persist/es/persistReducer";
+// import persistReducer from "redux-persist/es/persistReducer";
 import { persistStore } from "redux-persist";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
 
-const persistedReducer = persistReducer(persistConfig, cartReducer);
+// const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
     reducer: {
