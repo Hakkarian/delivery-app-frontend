12b0d8bd2b3afb2ea66ab48928f11713297137dc
Hakkar <zugrutus@gmail.com>
2023-06-02 01:03:01 +0100

    loader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
+import { Toaster } from "react-hot-toast";
+import { Routes, Route } from 'react-router-dom';
+import AppBar from "./AppBar";
+import ErrorPage from 'pages/ErrorPage';
+import Loader from 'components/Loader/Loader';
 
-import {Routes, Route} from 'react-router-dom';
-import ShoppingPage from 'pages/ShoppingPage';
-import MainPage from 'pages/MainPage';
-import ShoppingCartPage from 'pages/ShoppingCartPage';
-import AppBar from './AppBar';
-import { Toaster } from 'react-hot-toast';
+const MainPage = lazy(() => import('pages/MainPage'));
+const ShoppingPage = lazy(() => import("pages/ShoppingPage"));
+const ShoppingCartPage = lazy(() => import("pages/ShoppingCartPage"));
 
 const App = () => {
   return (
     <>
       <Toaster />
       <AppBar />
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/cart" element={<ShoppingCartPage />} />
-      <Route path="/shop" element={<ShoppingPage />} />
-    </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/cart" element={<ShoppingCartPage />} />
+          <Route path="/shop" element={<ShoppingPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
